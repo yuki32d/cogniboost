950bb0b629f1f617c0a48f53ea10c9d8173501e9
yuki32d <162720084+yuki32d@users.noreply.github.com>
2026-03-02 12:38:01 +0530

    Update API baseURL to server IP for remote access

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'http://10.201.7.200:5000'
 });
 
 axiosInstance.interceptors.request.use((config) => {
